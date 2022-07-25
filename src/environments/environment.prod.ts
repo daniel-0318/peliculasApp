12da513670da030bbb541b0bf99c8ba552e5c30b
Daniel Ramirez <dani_0318@hotmail.com>
2022-07-25 00:31:21 -0500

    Slides implementados a la clase 146

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  url: 'https://api.themoviedb.org/3',
+  apiKey:'f74fd1d32ed221f63a5c5eecec393623',
+  imgPath: 'https://image.tmdb.org/t/p'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  url: 'https://api.themoviedb.org/3',
+  apiKey:'f74fd1d32ed221f63a5c5eecec393623',
+  imgPath: 'https://image.tmdb.org/t/p'
 };
 
 /*
